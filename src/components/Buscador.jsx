76b1d8aeaa8e023d90c040357969b53643ba37d5
SebastianFlores <berendicto@gmail.com>
2023-09-08 04:35:57 -0300

    se agrega filtro sort y se crea componente buscador

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Buscador() {
+export default function Buscador({ dataPokemon, setDataPokemon }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Función para manejar cambios en el campo de búsqueda.
+  const handleSearchChange = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
+
+    // Filtrar y ordenar los Pokémon en función del término de búsqueda y el ID.
+    const filtered = dataPokemon
+      .filter((pokemon) => {
+        return (
+          pokemon.id.toString().includes(value) ||
+          pokemon.name.toLowerCase().includes(value.toLowerCase()) ||
+          pokemon.type.toLowerCase().includes(value.toLowerCase())
+        );
+      })
+      .sort((a, b) => a.id - b.id); // Ordena por ID de menor a mayor.
+
+    setDataPokemon(filtered);
+  };
+
   return (
-    <div>Buscador</div>
-  )
+    <div className="mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Buscar por ID, nombre o tipo"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
 }
--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -2,53 +2,54 @@ import React, { useEffect, useState } from "react";
 import Buscador from "./Buscador";
 
 export default function MiApi() {
-    const API_HOST = "https://pokeapi.co/api/v2/pokemon?limit=151";
-    const [dataPokemon, setDataPokemon] = useState([]);
+  const API_HOST = "https://pokeapi.co/api/v2/pokemon?limit=649";
+  const [dataPokemon, setDataPokemon] = useState([]);
+  const [filteredPokemon, setFilteredPokemon] = useState([]);
 
-    const consultarApi = async () => {
-        const response = await fetch(API_HOST);
-        const { results } = await response.json();
+  const consultarApi = async () => {
+    const response = await fetch(API_HOST);
+    const { results } = await response.json();
 
-        const pokemones = results.map(async (pokemon) => {
-            const response = await fetch(pokemon.url);
-            const poke = await response.json();
+    const pokemones = results.map(async (pokemon) => {
+      const response = await fetch(pokemon.url);
+      const poke = await response.json();
 
-    return {
+      return {
         id: poke.id,
         name: poke.name,
         img: poke.sprites.other.dream_world.front_default,
         type: poke.types[0].type.name,
-};
+      };
     });
     setDataPokemon(await Promise.all(pokemones));
-};
+    setFilteredPokemon(await Promise.all(pokemones));
+  };
 
-useEffect(() => {
+  useEffect(() => {
     consultarApi();
-}, []);
+  }, []);
 
-const showPokemons = dataPokemon.map((pokemon) => (
-    <div key={pokemon.id}  className="col">   
-            <div className="card h-100" >
-            <img src={pokemon.img} className="card-img-top" alt="..." />
-            <div className="card-body text-center">
-            <h5 className="card-title">{pokemon.name}</h5>
-            </div>
-                <div className="card-footer text-center">   
-                <h6><span className="badge bg-secondary">{pokemon.type}</span></h6>
-                </div>
-            
-        
-    </div>
-    </div>
-));
-
-return (
-    <>
-        <Buscador/>
-        <div className="row row-cols-1 row-cols-xl-5 g-3">
-            {showPokemons}
+  const showPokemons = filteredPokemon.map((pokemon) => (
+    <div key={pokemon.id} className="col">
+      <div className="card h-100">
+        <img src={pokemon.img} className="card-img-top" alt="..." />
+        <div className="card-body text-center">
+          <h5 className="card-title">{pokemon.name}</h5>
+          <h5 className="card-title">ID:{pokemon.id}</h5>
         </div>
+        <div className="card-footer text-center">
+          <h6>
+            <span className="badge bg-secondary">{pokemon.type}</span>
+          </h6>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <>
+      <Buscador dataPokemon={dataPokemon} setDataPokemon={setFilteredPokemon} />
+      <div className="row row-cols-1 row-cols-xl-5 g-3">{showPokemons}</div>
     </>
-);
+  );
 }
